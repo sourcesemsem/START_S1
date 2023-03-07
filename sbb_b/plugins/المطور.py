6e71cc6a2f57bee2ac183beb8e8d170376602927
SOURCE SPIDER <117318674+SOURCE-SPIDER@users.noreply.github.com>
2023-03-07 21:51:31 +0200

    Add files via upload

---
+++ sbb_b/plugins/المطور.py
@@ -0,0 +1,65 @@
+from telethon import Button, events
+
+from sbb_b import sbb_b 
+
+from ..Config import Config
+
+ROZ_PIC = "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg"
+RAZAN = Config.TG_BOT_USERNAME
+ROZ_T = (
+    f"**مطورين سورس سبايدر **\n"
+  
+)
+
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+
+    @tgbot.on(events.InlineQuery)
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        await bot.get_me()
+        if query.startswith("المطور") and event.query.user_id == bot.uid:
+            buttons = [
+                [
+                    Button.url("أسـامـة 🕷", "https://t.me/WWWL5"),
+                    Button.url("مـيـدو 🕷", "https://t.me/U_Y_4_M"),
+                    Button.url("الـقـناة 🕷", "https://t.me/EE_20"),
+                    Button.url("الجروب 🕷", "https://t.me/EE_47"),
+                    
+                ]
+            ]
+            
+            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
+                result = builder.photo(
+                    ROZ_PIC, text=ROZ_T, buttons=buttons, link_preview=False
+                )
+            elif ROZ_PIC:
+                result = builder.document(
+                    ROZ_PIC,
+                    title="JMTHON - sbb_b",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            else:
+                result = builder.article(
+                    title="JMTHON - sbb_b",
+                    text=ROZ_T,
+                    buttons=buttons,
+                    link_preview=False,
+                )
+            await event.answer([result] if result else None)
+
+
+@sbb_b.ar_cmd(pattern="المطور")
+async def repo(event):
+    RR7PP = Config.TG_BOT_USERNAME
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await bot.inline_query(WWWL5, "المطور")
+    await response[0].click(event.chat_id)
+    await event.delete()
+
+
+# edit by ~ @RR77R
